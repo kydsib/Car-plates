9577da642b918adcd805369caf3ca9992bf13a9a
kydsib <simonas.kersulis@gmail.com>
2020-07-30 11:22:14 +0300

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InputComponent } from './input/input.component';
+import { CarPlatesListComponent } from './car-plates/car-plates-list/car-plates-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InputComponent },
+  { path: 'list', component: CarPlatesListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './header/header.component';
@@ -20,7 +20,7 @@ import { CarPlateItemComponent } from './car-plates/car-plates-list/car-plate-it
     CarPlatesListComponent,
     CarPlateItemComponent
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
